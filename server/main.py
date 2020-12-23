adb365a2d4e40b298d86410481bb39375a7f8688
Jiwon Jeong <poiu8944@gmail.com>
2020-12-23 13:08:32 +0900

    대전 데이터 가져오기

---
+++ server/main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import requests
+import json
+import pandas as pd
+
+app = Flask (__name__)
+dataURL = 'https://coroname.me/getdata'
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/getdata')
+def getdata():
+    res = requests.get(dataURL, verify=False)
+    data = res.text
+    data = json.loads(data)['data']
+    daejeonData = getDaejeonData(data)
+    # print(daejeonData)
+    ret = daejeonData.to_json(force_ascii=False)
+
+    return ret
+
+
+def getDaejeonData(data):
+    df = pd.DataFrame.from_dict(data, orient='columns')
+    df = df.loc[df.region == '대전']
+    df_arrange = pd.DataFrame(df, columns=['visitedDate', 'latlng', 'address', 'place'])
+    df_arrange = df_arrange.T
+    return df_arrange
+
+if __name__ == "__main__":
+    app.run()
